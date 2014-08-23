9ba5de3b2b818ea2b7a9726291fb380fc4f113f4
Rory O'Keeffe <rorokeeffe@gmail.com>
2014-08-23 21:45:12 +0100

    Provision for img call in returnTo (login middleware function)

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use(function(req, res, next) {
 app.use(function(req, res, next) {
   // Remember original destination before login.
   var path = req.path.split('/')[1];
-  if (/auth|login|logout|signup|fonts|favicon/i.test(path)) {
+  if (/auth|login|logout|signup|img|fonts|favicon/i.test(path)) {
     return next();
   }
   req.session.returnTo = req.path;
